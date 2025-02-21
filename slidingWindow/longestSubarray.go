fcca830cbed339887ef8f381a16128b64023556d
realmelan <dingsong.net@gmail.com>
2025-02-20 21:59:08 -0800

    Create longestSubarray.go

---
+++ slidingWindow/longestSubarray.go
@@ -0,0 +1,61 @@
+/*
+1493. Longest Subarray of 1's After Deleting One Element
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
+
+func longestSubarray(nums []int) int {
+    n := len(nums)
+    i, j, cnt0, cnt1 := 0, 0, 0, 0
+    for j < n {
+        v := nums[j]
+        if v == 0 {
+            cnt0++
+        } else {
+            cnt1++
+        }
+        if cnt0 > 1 {
+            if nums[i] == 0 {
+                cnt0--
+            }
+            i++
+        }
+        j++
+    }
+
+    if cnt1 == 0 {
+        return 0
+    } else {
+        return j - i - 1
+    }
+}
