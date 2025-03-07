acfe7d83715f200f528b97578129020445ce44c2
realmelan <dingsong.net@gmail.com>
2025-03-06 22:23:01 -0800

    Create createSortedArray.go

---
+++ divideConquer/createSortedArray.go
@@ -0,0 +1,138 @@
+/*
+1649. Create Sorted Array through Instructions
+Solved
+Hard
+Topics
+Companies
+Hint
+Given an integer array instructions, you are asked to create a sorted array from the elements in instructions. You start with an empty container nums. For each element from left to right in instructions, insert it into nums. The cost of each insertion is the minimum of the following:
+
+The number of elements currently in nums that are strictly less than instructions[i].
+The number of elements currently in nums that are strictly greater than instructions[i].
+For example, if inserting element 3 into nums = [1,2,3,5], the cost of insertion is min(2, 1) (elements 1 and 2 are less than 3, element 5 is greater than 3) and nums will become [1,2,3,3,5].
+
+Return the total cost to insert all elements from instructions into nums. Since the answer may be large, return it modulo 109 + 7
+
+ 
+
+Example 1:
+
+Input: instructions = [1,5,6,2]
+Output: 1
+Explanation: Begin with nums = [].
+Insert 1 with cost min(0, 0) = 0, now nums = [1].
+Insert 5 with cost min(1, 0) = 0, now nums = [1,5].
+Insert 6 with cost min(2, 0) = 0, now nums = [1,5,6].
+Insert 2 with cost min(1, 2) = 1, now nums = [1,2,5,6].
+The total cost is 0 + 0 + 0 + 1 = 1.
+Example 2:
+
+Input: instructions = [1,2,3,6,5,4]
+Output: 3
+Explanation: Begin with nums = [].
+Insert 1 with cost min(0, 0) = 0, now nums = [1].
+Insert 2 with cost min(1, 0) = 0, now nums = [1,2].
+Insert 3 with cost min(2, 0) = 0, now nums = [1,2,3].
+Insert 6 with cost min(3, 0) = 0, now nums = [1,2,3,6].
+Insert 5 with cost min(3, 1) = 1, now nums = [1,2,3,5,6].
+Insert 4 with cost min(3, 2) = 2, now nums = [1,2,3,4,5,6].
+The total cost is 0 + 0 + 0 + 0 + 1 + 2 = 3.
+Example 3:
+
+Input: instructions = [1,3,3,3,2,4,2,1,2]
+Output: 4
+Explanation: Begin with nums = [].
+Insert 1 with cost min(0, 0) = 0, now nums = [1].
+Insert 3 with cost min(1, 0) = 0, now nums = [1,3].
+Insert 3 with cost min(1, 0) = 0, now nums = [1,3,3].
+Insert 3 with cost min(1, 0) = 0, now nums = [1,3,3,3].
+Insert 2 with cost min(1, 3) = 1, now nums = [1,2,3,3,3].
+Insert 4 with cost min(5, 0) = 0, now nums = [1,2,3,3,3,4].
+​​​​​​​Insert 2 with cost min(1, 4) = 1, now nums = [1,2,2,3,3,3,4].
+​​​​​​​Insert 1 with cost min(0, 6) = 0, now nums = [1,1,2,2,3,3,3,4].
+​​​​​​​Insert 2 with cost min(2, 4) = 2, now nums = [1,1,2,2,2,3,3,3,4].
+The total cost is 0 + 0 + 0 + 0 + 1 + 0 + 1 + 0 + 2 = 4.
+ 
+
+Constraints:
+
+1 <= instructions.length <= 105
+1 <= instructions[i] <= 105
+*/
+
+func createSortedArray(inst []int) int {
+    // divide & conquer: for each range, prepare a tuple of (value, index, strictly less)
+    // during merge, update the right half
+    mod := 1000000007
+    lt := make(map[int]int)
+    arr := merge(inst, 0, len(inst)-1)
+    for _, a := range arr {
+        lt[a[1]] = a[2]
+    }
+
+    var res int
+    eq := make(map[int]int)
+    for i, num := range inst {
+        res += min(lt[i], max(0, i-lt[i]-eq[num]))
+        res %= mod
+        eq[num]++
+    }
+    return res
+}
+
+func merge(nums []int, l, r int) [][]int {
+    if l >= r {
+        return [][]int{{nums[l], l, 0}}
+    }
+    m := (l+r)/2
+    left := merge(nums, l, m)
+    right := merge(nums, m+1, r)
+    i, ni := 0, len(left)
+    j, nj := 0, len(right)
+    var res [][]int
+    for j < nj {
+        for i < ni && left[i][0] < right[j][0] {
+            res = append(res, left[i])
+            i++
+        }
+        right[j][2] += i
+        res = append(res, right[j])
+        j++
+    }
+    res = append(res, left[i:ni]...)
+    return res
+}
+
+func createSortedArray_bit(instructions []int) int {
+    N, mod := 100000, 1000000007
+    bit := make([]int, N+1)
+    var res int
+    for i, num := range instructions {
+        cntle := query(bit, N+1, num)
+        cntlt := query(bit, N+1, num-1)
+        cntgt := i - cntle
+        res += min(cntgt, cntlt)
+        res %= mod
+
+        update(bit, N+1, num)
+    }
+    return res
+}
+
+func lsb(x int) int {
+    return x & (-x)
+}
+func update(bit []int, N, id int) {
+    for id < N {
+        bit[id]++
+        id += lsb(id)
+    }
+}
+func query(bit []int, N, id int) int {
+    var res int
+    for id > 0 {
+        res += bit[id]
+        id -= lsb(id)
+    }
+    return res
+}
