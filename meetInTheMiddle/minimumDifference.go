d6c30797bac27b3714f45582512a487a9ec6b126
realmelan <dingsong.net@gmail.com>
2025-03-09 18:29:19 -0700

    Update minimumDifference.go

--- meetInTheMiddle/minimumDifference.go
+++ meetInTheMiddle/minimumDifference.go
@@ -40,3 +40,57 @@ nums.length == 2 * n
 -107 <= nums[i] <= 107
 */
 
+func minimumDifference(nums []int) int {
+    total, n := 0, len(nums)
+    for _, num := range nums {
+        total += num
+    }
+
+    target, res := total/2, math.MaxInt
+    if total < 0 {
+        target = (total-1)/2
+    }
+    left := subsetSums(nums[0:n/2])
+    right := subsetSums(nums[n/2:n])
+    for i, sums := range left {
+        other := right[n/2-i]
+
+        j, jn := 0, len(other)
+        k := len(sums)-1
+        for k >= 0 && j < jn {
+            tsum := other[j] + sums[k]
+            if tsum == target {
+                if total & 1 == 0 {
+                    return 0
+                }
+                res = min(res, total - tsum - tsum)
+                j++
+            } else if tsum > target {
+                res = min(res, tsum + tsum - total)
+                k--
+            } else {
+                res = min(res, total - tsum - tsum)
+                j++
+            }
+        }
+    }
+    return res
+}
+
+func subsetSums(nums []int) [][]int {
+    n := len(nums)
+    res := make([][]int, n+1)
+    for i := 0; i < 1<<n; i++ {
+        cnt, sum := 0, 0
+        for j := 0; j < n; j++ {
+            if (i>>j) & 1 > 0 {
+                sum += nums[j]
+                cnt++
+            }
+        }
+        res[cnt] = append(res[cnt], sum)
+    }
+    for i := range res {
+        sort.Ints(res[i])
+    }
+   
